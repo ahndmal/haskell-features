0d3e07abbcff7f9426e3b2b578d8770d0e1bcaf9
Andrii Maliuta <quadr988@gmail.com>
2022-11-22 19:41:52 +0200

    files

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,24 @@
 module Main where
 
+import System.IO
+import Control.Monad
+import Data.List
+
+
+processFiles :: Int -> Int
+processFiles a = do
+  handle <- openFile "/home/andrii/Documents/lorem" ++ a ++ ".txt" ReadMode
+  contents <- hGetContents handle
+  let contentsLength = length contents
+  let wordsCount = length $ words contents
+  hClose handle
+  wordsCount
+
 
 main :: IO ()
-main =
-  putStr "Hello, Haskell! You're using a function from another package!"
+main = do
+--  mapM_ processFiles [1..20]
+  let num = processFiles 2
+  print (show num)
+
+
