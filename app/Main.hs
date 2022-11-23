c0a553ef4bf0b2c7d5382ec04a9068cc6e393bb3
Andrii Maliuta <quadr988@gmail.com>
2022-11-23 17:17:17 +0200

    files

--- app/Main.hs
+++ app/Main.hs
@@ -3,22 +3,36 @@ module Main where
 import System.IO
 import Control.Monad
 import Data.List
+import System.Random
+
+--1 > g <- newStdGen
+-- > randomR (1, 10) g
+--2  > randomRIO (1, 10)
+
+main :: IO ()
+main = do
+--  mapM_ processFiles [1..20]
+  let res = map processFiles [1..11] -- processFiles
+  print $ show res
+
+generateLorem :: Int -> String
+generateLorem 0 = "lorem"
+generateLorem a = generateL
+  where lorem = "Lorem ipsum dolor sit amet consectetur adipiscing elit facilisis sociosqu pretium, congue tincidunt maecenas "
+        loremWords = words lorem
+        lorLength = length loremWords
+        lorNew = " " ++ loremWords !! a ++ " "
+        generateL = map (\i -> lorem !! randomRIO (1, i)) [1..lorLength]
 
 
-processFiles :: Int -> Int
+processFiles :: Int -> IO Int
 processFiles a = do
-  handle <- openFile "/home/andrii/Documents/lorem" ++ a ++ ".txt" ReadMode
+  let fileUrl = "/home/andrii/Documents/lorem" ++ show a ++ ".txt"
+  handle <- openFile fileUrl ReadMode
   contents <- hGetContents handle
   let contentsLength = length contents
   let wordsCount = length $ words contents
   hClose handle
   wordsCount
-
-
-main :: IO ()
-main = do
---  mapM_ processFiles [1..20]
-  let num = processFiles 2
-  print (show num)
 
 
